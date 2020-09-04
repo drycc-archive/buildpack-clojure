74dc2da53425c0e3a341deced630b78ab5a8543e
duanhongyi <duanhongyi@doopai.com>
2020-09-04 12:56:24 +0800

    chore(buildpacks): change buildpacks url

--- etc/upgrade-lein.sh
+++ etc/upgrade-lein.sh
@@ -6,7 +6,7 @@ LEIN_VERSION=${1}
 LEIN_FILE="/tmp/leiningen-$LEIN_VERSION-standalone.jar"
 
 echo "---> Downloading Leiningen version $LEIN_VERSION..."
-curl -o $LEIN_FILE --location https://buildpacks.oss-accelerate.aliyuncs.com/github.com/technomancy/leiningen/releases/download/$LEIN_VERSION/leiningen-$LEIN_VERSION-standalone.zip
+curl -o $LEIN_FILE --location http://buildpacks.drycc.cc/github.com/technomancy/leiningen/releases/download/$LEIN_VERSION/leiningen-$LEIN_VERSION-standalone.zip
 
 echo "---> Uploading leiningen-$LEIN_VERSION-standalone.jar to S3"
 aws s3 cp $LEIN_FILE s3://lang-jvm --profile lang-jvm --acl public-read
